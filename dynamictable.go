6c3c1d8bc68e4fd9ec9eaa896a1c0dee6e36f014
renthraysk <renthraysk@gmail.com>
2023-05-05 13:24:55 +0100

    Add initial eviction code

--- dynamictable.go
+++ dynamictable.go
@@ -2,16 +2,87 @@ package quack
 
 import (
 	"errors"
+	"math/bits"
 )
 
 type DT struct {
-	headers []headerField
-	base    uint64
-	head    int
+	headers  []headerField
+	base     uint64
+	capacity uint64
+	size     uint64
+	evicted  uint64
 }
 
+func New(maxCapacity uint64) DT {
+	return DT{
+		capacity: maxCapacity,
+	}
+}
+
+func (dt *DT) maxEntries() uint64 { return dt.capacity / 32 }
+
 func (dt *DT) lookup(name, value string) (uint64, match) {
-	return 0, matchNone
+	i := len(dt.headers) - 1
+	for i >= 0 && dt.headers[i].Name != name {
+		i--
+	}
+	if i < 0 || i >= len(dt.headers) {
+		return 0, matchNone
+	}
+	if dt.headers[i].Value == value {
+		return uint64(i), matchNameValue
+	}
+	j := i - 1
+	for j >= 0 && dt.headers[j].Name != name || dt.headers[j].Value != value {
+		j--
+	}
+	if j < 0 {
+		return uint64(i), matchName
+	}
+	return uint64(j), matchNameValue
+}
+
+// evict evicts headerFields until size is less than targetSize, returns the
+// number of headerFields evicted.
+func (dt *DT) evict(targetSize uint64) (uint64, bool) {
+	var i int
+
+	size := dt.size
+	for i < len(dt.headers) && size > targetSize {
+		size -= dt.headers[i].size()
+		i++
+	}
+	if i == 0 {
+		return 0, true
+	}
+	if i >= len(dt.headers) {
+		return 0, false
+	}
+	n := uint64(i)
+	if ec, c := bits.Add64(dt.evicted, n, 0); c != 0 {
+		return 0, false
+	} else {
+		dt.evicted = ec
+	}
+	dt.headers = append(dt.headers[:0], dt.headers[n:]...)
+	dt.size = size
+	return n, true
+}
+
+func (dt *DT) insert(name, value string) (int, bool) {
+	s := size(name, value)
+	if s > dt.capacity-dt.size {
+		if s > dt.capacity {
+			return 0, false
+		}
+		if _, ok := dt.evict(dt.capacity - s); !ok {
+			return 0, false
+		}
+	}
+	i := len(dt.headers)
+	dt.headers = append(dt.headers, headerField{name, value})
+	dt.size += s
+	return i, true
 }
 
 // https://www.rfc-editor.org/rfc/rfc9204.html#name-encoded-field-section-prefi
@@ -20,8 +91,7 @@ func (dt *DT) appendFieldSectionPrefix(p []byte, reqInsertCount uint64) []byte {
 
 	// https://www.rfc-editor.org/rfc/rfc9204.html#name-required-insert-count
 	if reqInsertCount > 0 {
-		maxEntries := uint64(cap(dt.headers)) / 32
-		insertCount = (reqInsertCount % (2 * maxEntries)) + 1
+		insertCount = (reqInsertCount % (2 * dt.maxEntries())) + 1
 	}
 	p = appendVarint(p, insertCount, 0xFF, 0)
 
@@ -33,18 +103,6 @@ func (dt *DT) appendFieldSectionPrefix(p []byte, reqInsertCount uint64) []byte {
 		sign = 0b1000_0000
 	}
 	return appendVarint(p, base, 0b0111_1111, sign)
-}
-
-func (dt *DT) insert(name, value string) int {
-	i := dt.head
-	if len(dt.headers) < cap(dt.headers) {
-		// this never reallocs so cap() is const
-		dt.headers = append(dt.headers, headerField{name, value})
-	} else {
-		dt.headers[i] = headerField{name, value}
-	}
-	dt.head = (i + 1) % cap(dt.headers)
-	return i
 }
 
 func (dt *DT) nameIndex(index uint64) (string, error) {
--- quack.go
+++ quack.go
@@ -4,3 +4,11 @@ type headerField struct {
 	Name  string
 	Value string
 }
+
+func (hf headerField) size() uint64 {
+	return size(hf.Name, hf.Value)
+}
+
+func size(name, value string) uint64 {
+	return uint64(len(name)) + uint64(len(value)) + 32
+}
