1e4ac94c9b49c07717304b033c14e846d10c670b
renthraysk <renthraysk@gmail.com>
2023-05-25 12:44:31 +0100

    StringLiterals aren't just used in fieldlines

--- internal/inst/fieldline.go
+++ internal/inst/fieldline.go
@@ -55,24 +55,6 @@ func AppendLiteralName(p []byte, name string, neverIndex bool) []byte {
 	return ascii.AppendLower(p, name)
 }
 
-// AppendStringLiteral appends the QPACK encoded string literal s to p.
-func AppendStringLiteral(p []byte, s string, shouldHuffman bool) []byte {
-	const (
-		H = 0b1000_0000
-		M = 0b0111_1111
-	)
-
-	n := uint64(len(s))
-	if n > 2 && shouldHuffman {
-		if h := huffman.EncodeLength(s); h < n {
-			p = varint.Append(p, h, M, H)
-			return huffman.AppendString(p, s)
-		}
-	}
-	p = varint.Append(p, n, M, 0)
-	return append(p, s...)
-}
-
 func t(b bool, t byte) byte {
 	if b {
 		return t
